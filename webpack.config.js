5574208b6f9576601854c2cef180466ab82cbbaa
hero-kitamura <122727573+hero-kitamura@users.noreply.github.com>
2023-04-26 11:47:27 +0900

    Load images with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,51 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: './src/javascripts/main.js',
-    output:{
-        path: path.resolve(__dirname, './dist'),
-        filename: 'javascripts/main.js'
-    },
-    module:{
-        rules:[
-            {
-                test:/\.css/,
-                use:[
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader:'css-loader',
-                    },
-                ],
-            },
+  entry: "./src/javascripts/main.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "javascripts/main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
         ],
-    },
-    plugins:[
-        new MiniCssExtractPlugin({
-            filename: './stylesheets/main.css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/templates/index.html',
-        }),
-        new CleanWebpackPlugin(),
+      },
+      {
+        test: /\.(png|jpg)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
     ],
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./stylesheets/main.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  stats: {
+    children: true,
+  },
+};
